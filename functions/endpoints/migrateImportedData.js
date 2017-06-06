7533716fabd3b87de1ad343497b574974ab838a6
amoothart <amooth@gmail.com>
2017-06-06 10:52:53 -0500

    transfer from import collection to overwrite all

--- functions/endpoints/migrateImportedData.js
+++ functions/endpoints/migrateImportedData.js
@@ -3,38 +3,30 @@ const moment = require('moment')
 
 module.exports.migrate = function(context, complete, modules) {
   const userId = modules.requestContext.getAuthenticatedUserId()
+  const collectionName = context.body.collection
 
   const options = {
     useBl: false,
-    useUserContext: false
+    useUserContext: true
   }
   const dataStore = modules.dataStore(options)
-  const offline = dataStore.collection("CompressorOfflineEvents")
-  const online = dataStore.collection("CompressorOnlineEvents")
-  Promise.promisifyAll(offline)
-  Promise.promisifyAll(online)
+  const targetCollection = dataStore.collection(collectionName)
+  const importCollection = dataStore.collection("imported")
+  Promise.promisifyAll(targetCollection)
+  Promise.promisifyAll(importCollection)
 
-  online.findAsync().then(function(results) {
+  importCollection.findAsync().then(function(results) {
     var promises = []
-    results.forEach(function(onlineEntity) {
-      const timeSplits = onlineEntity.last_updated.split(/ |:/)
-      onlineEntity._kmd.lmt = moment(timeSplits[0]).add(timeSplits[1], "hours").add(timeSplits[2], "minutes")
-      onlineEntity._kmd.ect = onlineEntity._kmd.lmt
-      onlineEntity._acl = {"creator": userId}
-      delete onlineEntity.last_updated
-      promises.push(online.saveAsync(onlineEntity))
-
-      const offlineEntity = {
-        "_id": onlineEntity._id,
-        "_kmd": onlineEntity._kmd,
-        "_acl": onlineEntity._acl,
-        "operational_code": onlineEntity.operational_code,
-        "start_time": onlineEntity.start_time,
-        "end_time": onlineEntity.end_time,
-        "equip_id": onlineEntity.equip_id,
-        "downtime_code_id": onlineEntity.downtime_code_id
+    results.forEach(function(entity) {
+      if(collectionName == "CompressorEvents") {
+        const timeSplits = entity.last_updated.split(/ |:/)
+        entity._kmd.lmt = moment(timeSplits[0]).add(timeSplits[1], "hours").add(timeSplits[2], "minutes")
+        entity._kmd.ect = entity._kmd.lmt
+        entity._acl = {"creator": userId}
+        delete entity.last_updated
       }
-      promises.push(offline.saveAsync(offlineEntity))
+
+      promises.push(targetCollection.saveAsync(entity))
     });
     return Promise.all(promises)
   }).then(function(results) {
