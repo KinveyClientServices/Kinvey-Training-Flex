a0bbdf70028ffa85acec3d2d2462bef07f389648
amoothart <amooth@gmail.com>
2017-08-03 12:27:56 -0400

    create data with non-kinvey ids

--- functions/endpoints/savedata.js
+++ functions/endpoints/savedata.js
@@ -5,21 +5,23 @@ module.exports.savedata = function(context, complete, modules) {
 	console.log("echo context: " + JSON.stringify(context));
 	const options = {
 		useBl: true,
-		useUserContext: false
+		useUserContext: true
 	}
 	const store = modules.dataStore(options).collection('Todo');
 	const todo = {
+		"_id": "57186771a8b943fd316fffff",
 		"action": randomstring.generate(7),
 		"duedate": moment(),
 		"completed":false
 	}
-	var entity = modules.kinveyEntity.entity(todo);
-	entity._acl["gw"] = true;
+	var entity = modules.kinveyEntity.entity(todo)
+	entity._id = "57186771a8b943fd316fffff"
 
+	console.log("saving entity: " + JSON.stringify(entity));
 	store.save(entity, function(err, result) {
 		if(err) {
-			return complete().setBody(err).runtimeError().done();
+			return complete().setBody(err).runtimeError().done()
 		}
-		return complete().setBody("success").ok().next();
+		return complete().setBody(result).ok().next()
 	})
 }
